61275b74d0fc4a5c176c4be822328fcf08b9d295
Daniel Novacic <daniel.novacic92@gmail.com>
2023-12-03 19:49:33 +0100

    Add day 3

---
+++ Day3/Gear.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+	public class Gear
+	{
+		public int FirstGear { get; set; }
+		public int SecondGear { get; set; }
+	}
+}
